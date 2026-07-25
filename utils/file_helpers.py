dd7355742ca631e9abdf5ec462a60f360c826775
8a-ma <e0as0k98x001b@gmail.com>
2026-07-25 11:36:03 -0400

    feat: save and load file helpers

---
+++ utils/file_helpers.py
@@ -0,0 +1,53 @@
+import json
+import pickle
+import pandas as pd
+from pathlib import Path
+from sklearn.pipeline import Pipeline
+
+
+def save_df_to_csv(df: pd.DataFrame, output_file: Path) -> None:
+    assert output_file.parent.exists()
+
+    df.to_csv(output_file, index=False, encoding='utf-8')
+
+    assert output_file.exists()
+
+
+def load_csv_to_df(input_file: Path) -> pd.DataFrame:
+    assert input_file.exists()
+
+    df: pd.DataFrame = pd.read_csv(input_file)
+
+    assert not df.empty()
+
+    return df
+
+
+def save_baseline_model(model: Pipeline, path: Path) -> None:
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(path, 'wb') as f:
+        pickle.dump(model, f)
+
+    assert path.exists()
+
+
+def load_baseline_model(path: Path) -> Pipeline:
+    assert path.exists()
+
+    with open(path, 'rb') as f:
+        model: Pipeline = pickle.load(f)
+
+    assert isinstance(model, Pipeline)
+
+    return model
+
+
+def save_dict_to_json(data: dict, path: Path, indent: int = 4) -> None:
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=indent)
+
+
+    assert path.exists()
